1f1bec27b4ee5f7c623dbf27f60934db89f9176e
Mahzabin-Momo <139198649+Mahzabin-Momo@users.noreply.github.com>
2024-07-14 00:59:21 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+ include "layout/header.php";
+ ?>     
+
+ <div style="Background-color: #08618d;">
+  <div class="container text-white py-5">
+    <div class="row align-items-center g-5">
+      <div class="col-mid-6">
+        <h1 class="mb-5 display-2"><strong>Best shop of electronics</strong></h1>
+        <p>
+          Find a large selection of newest electronic devices from most popular
+          brands and with affordable prices.
+      </p>
+      </div>
+      <div class="col-md-6 text-center">
+        <img src="images/phone.png.png" class="img-fluid" alt="hero" />
+      </div>
+      </div>
+      </div>
+      </div>
+         
+<?php
+include "layout/footer.php";
+?>
