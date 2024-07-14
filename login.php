9a0d7aeebffce56c0bbf63498195f84d28a7b0f7
Mahzabin-Momo <139198649+Mahzabin-Momo@users.noreply.github.com>
2024-07-14 11:19:26 +0600

    login.php

--- login.php
+++ login.php
@@ -5,26 +5,29 @@ if (isset($_SESSION["email"])) {
     header("location: /index.php");
     exit;
 }
+
 $email = "";
 $error = "";
-if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'];
     $password = $_POST['password'];
-    if (empty($email) || empty($password)){
+
+    if (empty($email) || empty($password)) {
         $error = "Email and password required!";
-    }
-    else{
+    } else {
         include "tools/db.php";
         $dbConnection = getDatabaseConnection();
         $statement = $dbConnection->prepare(
-            "SELECT id, first_name, last_name, phone, address, passowrd, created_at FROM users WHERE email = ?"
+            "SELECT id, first_name, last_name, phone, address, password, created_at FROM users WHERE email = ?"
         );
         $statement->bind_param('s', $email);
         $statement->execute();
         $statement->bind_result($id, $first_name, $last_name, $phone, $address, $stored_password, $created_at);
-        if ($statement->fetch()){
-            if (password_verify($password, $stored_password)){
-                $_SESSION["id"] = $insert_id;
+
+        if ($statement->fetch()) {
+            if (password_verify($password, $stored_password)) {
+                $_SESSION["id"] = $id;
                 $_SESSION["first_name"] = $first_name;
                 $_SESSION["last_name"] = $last_name;
                 $_SESSION["email"] = $email;
@@ -44,39 +47,38 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
 <div class="container py-5">
     <div class="mx-auto border shadow p-4" style="width: 400px">
-<h2 class="text-center mb-4">Login</h2>
-<hr />
-<?php if (!empty($error)) { ?>
-    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-        <strong><?= error ?></strong>
-        <button type = "button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <h2 class="text-center mb-4">Login</h2>
+        <hr />
+        <?php if (!empty($error)) { ?>
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <strong><?= $error ?></strong>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        <?php } ?>
+
+        <form method="post">
+            <div class="mb-3">
+                <label class="form-label">Email</label>
+                <input class="form-control" name="email" value="<?= $email ?>" />
+            </div>
+
+            <div class="mb-3">
+                <label class="form-label">Password</label>
+                <input class="form-control" type="password" name="password" />
+            </div>
+
+            <div class="row mb-3">
+                <div class="col d-grid">
+                    <button type="submit" class="btn btn-primary">Log in</button>
+                </div>
+                <div class="col d-grid">
+                    <a href="/index.php" class="btn btn-outline-primary">Cancel</a>
+                </div>
+            </div>
+        </form>
     </div>
-
-<?php } ?>
-
-<form method="post>
-   <div class="mb-3">
-    <label class="form-label">Email</label>
-    <input class="form-control" name="email" value="<?= $email ?>" />
 </div>
 
-<div class="mb-3">
-    <label class="form-label">Password</label>
-    <input class="form-control" type="password" name="password />
-</div>
-
-<div class="row mb-3">
-    <div class="col d-grid">
-        <button type="submit" class="btn btn-primary">Log in</button>
-
-</div>
-<div class="col d-grid">
-    <a href="/index.php" class="btn btn-outline-primary">Cancel</a>
-</div>
-</div>
-    </form>
-</div>
-</div>
 <?php
 include "layout/footer.php";
 ?>
